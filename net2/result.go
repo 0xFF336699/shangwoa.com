1f807ab280187feaf8a57f956e85428e654efba7
0xff336699 <0xff336699@gmail.com>
2019-08-21 21:02:31 +0800

    x

--- net2/result.go
+++ net2/result.go
@@ -1,11 +1,45 @@
-package net
+package net2
+
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type ErrorInfo struct {
 	Msg string `json:"msg"`
 	Err error `json:"err"`
 }
+type DataInfo struct {
+	Type string `json:"type"`
+	Data interface{}
+}
 type Result struct {
 	IsOK bool `json:"isOk"`
-	Data interface{} `json:"data"`
+	DataInfo *DataInfo `json:"dataInfo"`
 	ErrorInfo *ErrorInfo `json:"errorInfo"`
+}
+
+func ResponseWrite(err error, data interface{}, w http.ResponseWriter) bool {
+	res := &Result{}
+	if err !=nil{
+		e := &ErrorInfo{Msg:err.Error(), Err:err}
+		res.ErrorInfo = e
+		res.IsOK = false
+	}else {
+		res.IsOK = true
+	}
+	if d, ok := data.(*DataInfo); ok{
+		res.DataInfo = d
+	}else{
+		res.DataInfo = &DataInfo{Data:data}
+	}
+	b, e := json.Marshal(res)
+	if e != nil{
+		w.Write([]byte(`{"isOK":false,"errorInfo":{"msg":"parse json error"}`));
+	}else{
+		w.Write(b)
+		return true
+	}
+
+	return false
 }
