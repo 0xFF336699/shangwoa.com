14a25f8012d670d9411dda3c96a164a548a3b43b
0xff336699 <0xff336699@gmail.com>
2019-10-13 19:15:33 +0800

    x

--- time2/layout.go
+++ time2/layout.go
@@ -1,5 +1,10 @@
 package time2
 
+import "time"
 const (
 	FormatOnlyTime = "2006-01-02 15:04:05"
 )
+var CNLocation *time.Location
+func init() {
+	CNLocation, _= time.LoadLocation("Asia/Chongqing")
+}
