edff8311d1cfa25e36167be8b58f7d5bc413f76c
0xff336699 <0xff336699@gmail.com>
2020-01-06 10:46:28 +0800

    2020年01月 6日 10:46:28

--- utils/retry/to-retry.go
+++ utils/retry/to-retry.go
@@ -1,7 +1,22 @@
 package retry
 
+import (
+	"errors"
+	"time"
+)
+
 type exec func ()(error, interface{});
 type onError func(err error, count int)bool
+
+func CreateSleepTimeOnError(d time.Duration, maxCount int) onError {
+	return func(err error, count int) bool {
+		if count > maxCount {
+			return false
+		}
+		time.Sleep(d)
+		return true
+	}
+}
 func ToRetry(exec exec, onError onError)(err error, res interface{})  {
 	i := 0
 	for ;;i++ {
@@ -14,4 +29,11 @@ func ToRetry(exec exec, onError onError)(err error, res interface{})  {
 			return
 		}
 	}
+}
+
+func testToRetry() {
+	err, res := ToRetry(func() (err error, i interface{}) {
+		return errors.New("test"), i
+	}, CreateSleepTimeOnError(time.Second * 1, 20));
+	print(err, res)
 }
