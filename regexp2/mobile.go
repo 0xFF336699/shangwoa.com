51bd883f2334449f95fe92db0993f7e91d728fe1
shangwoa <shangwoa@gmail.com>
2020-09-24 13:34:48 +0800

    Thu Sep 24 13:34:48     2020

--- regexp2/mobile.go
+++ regexp2/mobile.go
@@ -3,11 +3,11 @@ package regexp2
 import "regexp"
 
 func IsMobile(mobile string) bool {
-	re := regexp.MustCompile(`^1[345678][0-9]{9}$`)
+	re := regexp.MustCompile(`^1[0123456789][0-9]{9}$`)
 	return re.MatchString(mobile)
 }
 
 func HasMobile(mobile string) bool {
-	re := regexp.MustCompile(`1[345678][0-9]{9}`)
+	re := regexp.MustCompile(`1[0123456789][0-9]{9}`)
 	return re.MatchString(mobile)
 }
